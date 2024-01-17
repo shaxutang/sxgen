3fc7a7ee71150d709b8b07c8dc8b54beda01c9a7
shaxutang <olaysunju@163.com>
2024-01-17 21:00:53 +0800

    chore: rename

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 import { fileURLToPath } from 'url'
 import { loopPath } from './util'
 
-export const WORKSPACE_NAME = '.sgen'
+export const WORKSPACE_NAME = '.sxgen'
 export const WORKSPACE_PATH =
   loopPath(process.cwd(), WORKSPACE_NAME) ||
   fileURLToPath(new URL(`../${WORKSPACE_NAME}`, import.meta.url))
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { program } from 'commander'
 import { version } from '../package.json'
 import gen from './gen'
 
-program.name('sgen').version(version)
+program.name('sxgen').version(version)
 
 program.argument('[name]', 'template name').action((name) => {
   gen({ name })
